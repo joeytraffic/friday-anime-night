fd70a5e778b0960da606e2d62f93e16587f6faa2
ttieman <tiemantanner@gmail.com>
2023-02-06 21:26:51 -0700

    populated data to be displayed on the screen from spoontacular

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var spoonacularRecipeTitle = $('#food-title');
+var spoonacularImage = $("#food-image");
+var spoonacularLink = $("#food-link");
+var spoonacularSummary = $("#food-summary");
 $(document).ready(function(){  // keep everything contained in here this ensures jquery is ready before any other code is executed
     
     
@@ -8,12 +12,21 @@ $(document).ready(function(){  // keep everything contained in here this ensures
     function fetchSpoontacular(){//fetch spoontacular api
         const spoonacularUrl = "https://api.spoonacular.com/recipes/random/?";
         const spoonApiKey = "apiKey=ad5d29ecd2fe482aabd08b828d7593a3";
-        fetch(spoonacularUrl + spoonApiKey)
-        .then((response) => response.json())
-        .then((data) => console.log(data));  
+        
+        fetch(spoonacularUrl + spoonApiKey).then(function (response) { 
+            if(response.ok){
+        response.json()
+        .then(function (data) { 
+            console.log(data);
+            spoonacularRecipeTitle.text(data.recipes[0].title);
+            spoonacularLink.text("Click here for the recipe!").attr("href",data.recipes[0].sourceUrl);
+            spoonacularImage.attr("src",data.recipes[0].image);
+            spoonacularSummary.html(data.recipes[0].summary);
+         });
+               
+        }});
+
     }
-
-
         
     
     function fetchJikan(){}  //fetch jikan api 
